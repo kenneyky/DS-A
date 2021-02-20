d98ff5766b40a7605d689598680bdca249f439b4
kenneyky <kenneyky520@gmail.com>
2021-02-20 18:00:12 -0500

    Added Binary Search

---
+++ Tests/SearchingTests/BinarySearchTest.cs
@@ -0,0 +1,20 @@
+﻿using console.Searching;
+using Xunit;
+
+namespace Tests.SearchingTests
+{
+    public class BinarySearchTest
+    {
+        [Fact]
+        public void TestBinarySearch()
+        {
+            var A = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.True(binarySearch.Iterative(A, 5));
+            Assert.False(binarySearch.Iterative(A, 10));
+
+            Assert.True(binarySearch.Recursive(A, 5, 0, A.Length - 1));
+            Assert.False(binarySearch.Recursive(A, 10, 0, A.Length - 1));
+        }
+    }
+}
---
+++ console/Searching/BinarySearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console.Searching
+{
+    public class BinarySearch
+    {
+
+        public Boolean Iterative(int[] A, int x)
+        {
+            int left = 0;
+            int right = A.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + ((right - left) / 2);
+                if (A[mid] == x)
+                {
+                    return true;
+                }
+                else if (x < A[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Recursive(int[] A, int x, int left, int right)
+        {
+            if (left > right)
+            {
+                return false;
+            }
+
+            int mid = left + ((right - left) / 2);
+            if (A[mid] == x)
+            {
+                return true;
+            }
+            else if (x < A[mid])
+            {
+                return Recursive(A, x, left, mid - 1);
+            }
+            else
+            {
+                return Recursive(A, x, mid + 1, right);
+            }
+        }
+    }
+}
